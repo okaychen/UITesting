2cefd20356ebb8e65be942014569393d7e9488cc
okaychen <1440281580@qq.com>
2021-07-26 16:06:01 +0800

    feat: update router

--- index.js
+++ index.js
@@ -7,67 +7,57 @@ const Koa = require('koa');
 const app = module.exports = new Koa();
 
 // "database"
-
 const posts = [];
 
 // middleware
-
 app.use(logger());
-
 app.use(render);
-
 app.use(koaBody());
 
 // route definitions
-
 router.get('/', list)
-  .get('/post/new', add)
-  .get('/post/:id', show)
-  .post('/post', create);
+    .get('/post/new', add)
+    .get('/post/:id', show)
+    .post('/post', create);
 
 app.use(router.routes());
 
 /**
  * Post listing.
  */
-
 async function list(ctx) {
-  await ctx.render('list', { posts: posts });
+    await ctx.render('index', { posts: posts });
 }
 
 /**
  * Show creation form.
  */
-
 async function add(ctx) {
-  await ctx.render('new');
+    await ctx.render('new');
 }
 
 /**
  * Show post :id.
  */
-
 async function show(ctx) {
-  const id = ctx.params.id;
-  const post = posts[id];
-  if (!post) ctx.throw(404, 'invalid post id');
-  await ctx.render('show', { post: post });
+    const id = ctx.params.id;
+    const post = posts[id];
+    if (!post) ctx.throw(404, 'invalid post id');
+    await ctx.render('show', { post: post });
 }
 
 /**
  * Create a post.
  */
-
 async function create(ctx) {
-  const post = ctx.request.body;
-  const id = posts.push(post) - 1;
-  post.created_at = new Date();
-  post.id = id;
-  ctx.redirect('/');
+    const post = ctx.request.body;
+    const id = posts.push(post) - 1;
+    post.created_at = new Date();
+    post.id = id;
+    ctx.redirect('/');
 }
 
 // listen
-
 if (!module.parent) app.listen(3000, () => {
     console.log('server running at http://localhost:3000')
 });
